00f5d0654bde01d23a3d5e3c28df1ad84121e776
Ilyas Ilyasov <ilyasov000fp@gmail.com>
2022-10-27 19:21:56 +0200

    Add navigation and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Navigation } from "./components/Navigation";
+import { FavouritesPage } from "./pages/FavouritesPage";
+import { HomePage } from "./pages/HomePage";
 
 function App() {
-  return <div className="font-bold">Hello React</div>;
+  return (
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/favourites" element={<FavouritesPage />} />
+      </Routes>
+    </>
+  );
 }
-
 export default App;
