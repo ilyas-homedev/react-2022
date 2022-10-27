d4815cd6fe65f9b7659ea893102971e8193b6fa6
Ilyas Ilyasov <ilyasov000fp@gmail.com>
2022-10-27 19:56:21 +0200

    Add useSearchUsersQuery hook

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,7 @@
+import { useSearchUsersQuery } from "../store/github/github.api";
+
 export const HomePage = () => {
+  const { isLoading, isError, data } = useSearchUsersQuery("ilyas");
+
   return <h1>Home</h1>;
 };
