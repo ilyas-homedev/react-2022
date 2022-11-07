df512c478d7475446ba67e422963ce7e596d8f88
Ilyas Ilyasov <ilyasov000fp@gmail.com>
2022-11-07 09:35:39 +0100

    FavouritesPage.tsx: add layout

--- src/pages/FavouritesPage.tsx
+++ src/pages/FavouritesPage.tsx
@@ -1,3 +1,20 @@
+import { useAppSelector } from "../hooks/redux";
+
 export const FavouritesPage = () => {
-  return <div>fav</div>;
+
+  const {favourites} = useAppSelector(state => state.github);
+
+  if (favourites.length === 0) return <p className="text-center">No items.</p>
+
+  return (
+    <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
+      <ul className="list-none">
+        {favourites.map(f => {
+          return <li key={f}>
+            <a href={f} target="_blank">{f}</a>
+          </li>
+        })}
+      </ul>
+    </div>
+  )
 };
