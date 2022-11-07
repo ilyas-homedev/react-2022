f359dead5440aa116c9985fe45f5374964e74c1d
Ilyas Ilyasov <ilyasov000fp@gmail.com>
2022-11-07 09:35:07 +0100

    RepoCard.tsx: add buttons for favourites repos

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -1,6 +1,27 @@
+import { MouseEvent, useState } from "react"
+import { useActions } from "../hooks/actions"
+import { useAppSelector } from "../hooks/redux";
 import { IRepo } from "../models/models"
 
 export const RepoCard = ({repo}: {repo: IRepo}) => {
+
+  const {addFavourite, removeFavourite} = useActions();
+  const {favourites} = useAppSelector(state => state.github);
+
+  const [isFav, setIsFav] = useState(favourites.includes(repo.html_url));
+
+  const addToFavourite = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    addFavourite(repo.html_url)
+    setIsFav(true);
+  }
+
+  const removeFromFavourite = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    removeFavourite(repo.html_url);
+    setIsFav(false);
+  }
+
   return (
     <div className="border py-3 px-5 rounded mb-2 hover:shadow-md hover:bg-gray-100 transition-all">
       <a href={repo.html_url} target="_blank">
@@ -10,6 +31,14 @@ export const RepoCard = ({repo}: {repo: IRepo}) => {
           Watchers: <span className="font-bold">{repo.watchers}</span>
         </p>
         <p className="text-sm font-thin">{repo?.description}</p>
+
+        {!isFav && <button className="py-2 px-4 bg-yellow-400 mr-2 rounded hover:shadow-md transition-all"
+          onClick={addToFavourite}
+        >Add</button>}
+
+        {isFav && <button className="py-2 px-4 bg-red-400 rounded hover:shadow-md transition-all"
+          onClick={removeFromFavourite}
+        >Remove</button>}
       </a>
     </div> 
   )
